5b01a2efd6ecebb03252530b0320259b27d4f3a2
JUNGJAEHOON <tree34005@gmail.com>
2022-11-04 13:12:21 +0900

    edit deci

--- deci2bit.py
+++ deci2bit.py
@@ -1,25 +1,41 @@
 import numpy as np
 
+#, max_len
+def to_binary(number, max_len):
+    bit_list = []
+    num_bits = number.bit_length()
+    bits = "{0:b}".format(number, '#b')
+    for i in range(num_bits):
+        if bits[i] == '0':
+            bit_list.append(0)
+        else:
+            bit_list.append(1)
 
-def to_binary(number):
-    answer = "{0:b}".format(number, '#b')
-    return answer, len(answer)
+    if num_bits < max_len:
+        for i in range(max_len-num_bits):
+            bit_list.insert(0, 0)
+
+    return bit_list
 
 def deci2bit(input):
-    bits = []
+    input_bits = []
     row, col = np.shape(input)
-    _, max_len = to_binary(np.max(input))
+    max_len = int(np.max(input)).bit_length()
 
     for i in range(row):
         for j in range(col):
-            bits_, len_ = to_binary(input[i][j])
-            if len_ < max_len:
-                bits_ = bits_.zfill(max_len)
-
-            bits.append(bits_)
-    return bits
+            bits_ = to_binary(int(input[i][j]), max_len)
+            input_bits.append(bits_)
+    return np.reshape(np.array(input_bits), (-1, 1))
 
 
-inp = np.array([[1, 255, 0], [1, 100, 200]])
+inp =  np.random.randint(100, 130, size=(9, 10))
+
 print(inp)
 print(deci2bit(inp))
+f = open("C:/Users/Jaehoon/Desktop/test.txt", 'w')
+f.write(str(deci2bit(inp)))
+f.close()
+
+
+
