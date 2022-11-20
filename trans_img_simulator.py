f11f0adad31bda381e321412b2e8d44806369b1e
HyeongeonYu <yuhyeongeon@gmail.com>
2022-11-20 23:02:49 +0900

    add ex

--- trans_img_simulator.py
+++ trans_img_simulator.py
@@ -12,13 +12,20 @@ inp_file_dir = 'Lenna.png'
 source_coding_type = "NoCompression"
 modulation_scheme = "BPSK"
 mu = 0
-std = 1
+std = 0
 
-name_window = 'noise test'
+Tx_window = 'noise test'
+Rx_window = "Rx result"
+
 track_bar_name = '표준편차= value/50'
 
-cv2.namedWindow(name_window)
-cv2.createTrackbar(track_bar_name, name_window, std, 200, nothing)
+cv2.namedWindow(Tx_window)
+img_noise = communicationsystem.inp_with_noise(inp_file_dir, source_coding_type, modulation_scheme,
+                                                   mu, std)
+cv2.imshow(Tx_window, img_noise)
+
+cv2.namedWindow(Rx_window)
+cv2.createTrackbar(track_bar_name, Rx_window, std, 200, nothing)
 
 blue = (255, 0, 0)
 green= (0, 255, 0)
@@ -27,7 +34,7 @@ white= (255, 255, 255)
 font =  cv2.FONT_HERSHEY_PLAIN
 
 while(True):
-    std = cv2.getTrackbarPos(track_bar_name, name_window) / 50
+    std = cv2.getTrackbarPos(track_bar_name, Rx_window) / 50
     if std==0 :
         SNR_txt = "infinity"
     else:
@@ -38,7 +45,7 @@ while(True):
                                                    mu, std)
     cv2.putText(img_noise, SNR_txt, (380, 40), font, 2, red, 3)
 
-    cv2.imshow(name_window, img_noise)
+    cv2.imshow(Rx_window, img_noise)
 
     if cv2.waitKey(100)&0xFF == 27:
         break
